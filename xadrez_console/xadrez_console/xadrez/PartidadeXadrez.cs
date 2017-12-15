8211cf28ef2c780c64ebbf5c70adb34ef2c03c25
@AlexDeusdete <alexdeusdete@hotmail.com>
2017-12-15 20:05:32 -0200

    Promocao

--- xadrez_console/xadrez_console/xadrez/PartidadeXadrez.cs
+++ xadrez_console/xadrez_console/xadrez/PartidadeXadrez.cs
@@ -201,6 +201,21 @@ namespace xadrez
                 throw new TabuleiroException("Você não pode se colocar em Cheque");
             }
 
+            Peca p = tab.peca(destino);
+
+            //#jogadaEspecial promocao
+            if (p is Peao)
+            {
+                if ((p.cor == Cor.Branca && destino.linha == 0)||(p.cor == Cor.Preta && destino.linha == 7))
+                {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
             if (estaEmXeque(adversaria(jogadorAtual)))
                 xeque = true;
             else
@@ -213,8 +228,6 @@ namespace xadrez
                 turno++;
                 mudaJogador();
             }
-
-            Peca p = tab.peca(destino);
 
             //#jogadaEspecial en passant
             if (p is Peao && (destino.linha == origem.linha - 2 || destino.linha == origem.linha + 2))
